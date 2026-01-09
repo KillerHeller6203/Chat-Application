09dbe24272b034955d2bff9602293a0fcf7755f5
Rickzz y <duttarick6203@gmail.com>
2026-01-10 01:19:57 +0530

    Initial commit: realtime chat application

---
+++ realtime-chat-backend/src/main/java/com/rickzzyy/realtimechat/repositories/RoomRepository.java
@@ -0,0 +1,10 @@
+package com.rickzzyy.realtimechat.repositories;
+
+
+import com.rickzzyy.realtimechat.entities.Room;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface RoomRepository extends MongoRepository<Room, String> {
+    //get room using room id
+    Room findByRoomId(String roomId);
+}
